1a2d363fb7f84b7a55fe68ff1bec890e7e6ef280
KryptixGG <86965721+KryptixGG@users.noreply.github.com>
2024-05-08 03:02:34 +0200

    Add files via upload
    Added comments. Kinda simple ones too, because its my first addon.

--- DeleteSoulShards/DeleteSoulShards.lua
+++ DeleteSoulShards/DeleteSoulShards.lua
@@ -3,7 +3,7 @@ SLASH_DELETESOULSHARDSSTATUS1, SLASH_DELETESOULSHARDSSTATUS2 = '/dssstatus', '/d
 SLASH_DELETESOULSHARDSMAX1, SLASH_DELETESOULSHARDSMAX2, SLASH_DELETESOULSHARDSMAX3 = '/dssmax', '/deletesoulshardsmax', '/deletesoulshardsmaximum'
 
 
-
+--this gets called, when one of the registered events fires
 local function OnEvent(self, event, ...)
 	if event == "ADDON_LOADED" then
 		DeleteSoulShards_InitVars()
@@ -11,10 +11,11 @@ local function OnEvent(self, event, ...)
 	
 	local shardsToDelete = GetItemCount(6265) - DeleteSoulShards['MaxAmount']
     
+	--cycles through all bags
     for x = 0,4 do
-        
+        --cycles through all slots in that bag
         for y = 1, GetContainerNumSlots(x) do
-            
+            --if there are too many shards, it deletes 1, when it finds 1
             if (shardsToDelete > 0) then
                 local l = GetContainerItemID(x,y)
                 
@@ -38,11 +39,13 @@ function SlashCmdList.DELETESOULSHARDSSTATUS(msg, editbox)
 end
 
 function SlashCmdList.DELETESOULSHARDSMAX(msg, editbox)
-
+	
+	--checks if it was given a number and makes it an integer, if it had decimals. Then checks if the number makes sense >=0 and <=32
 	if tonumber(msg) then
 		local intMsg = math.floor(tonumber(msg))
 		
 		if intMsg >= 0 and intMsg <= 32 then
+			--sets saved Variable value to new value
 			DeleteSoulShards['MaxAmount'] = intMsg
 			print("Maximum amount of Soul Shards set to: "..DeleteSoulShards['MaxAmount']..".")
 			OnEvent()
@@ -55,6 +58,7 @@ function SlashCmdList.DELETESOULSHARDSMAX(msg, editbox)
 	
 end
 
+--creates a table for values if there is none and sets the default Value of the 'MaxAmount', if there is none
 function DeleteSoulShards_InitVars()
 	if DeleteSoulShards == nil or type(DeleteSoulShards) ~= "table" then
 		DeleteSoulShards = {}
@@ -64,7 +68,7 @@ function DeleteSoulShards_InitVars()
 	end
 end
 
-
+-- this has to be last, as functions need to be defined, before they can be referenced
 local f = CreateFrame("Frame")
 f:RegisterEvent("BAG_UPDATE")
 f:RegisterEvent("ADDON_LOADED")
